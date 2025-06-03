b8fecbbe28268293670c50416bc676d34f47ad34
byunn90 <kayhanturk90@Live.com>
2025-06-03 13:02:38 +1000

    setting up objects

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from constants import *
 import pygame
+
 def main():
+    print("Starting Asteroids")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while True:
-    	for event in pygame.event.get():
-    		if event.type == pygame.QUIT:
-        		return
-	    	screen.fill((0, 0, 0))
-	    	pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
-  main()
+    main()
+
---
+++ player.py
@@ -0,0 +1,5 @@
+from circle_shape import CircleShape
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y)
