1e3bc7d34ba9b178eb7995555be534d69c9c48d4
byunn90 <kayhanturk90@Live.com>
2025-06-05 14:22:48 +1000

    new class asteroid

--- main.py
+++ main.py
@@ -7,20 +7,25 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    #Create Groups
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    #Setup Display 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # ✅ Create player object here
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
         screen.fill("black")
-        player.draw(screen)  # ✅ This will now work
+        updateable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
