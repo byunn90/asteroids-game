5b97216ba873444a075b7a0809b6479f087743e4
byunn90 <kayhanturk90@Live.com>
2025-06-06 15:05:43 +1000

    asteroids are working

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from constants import *
 from player import Player
 import pygame
-
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 def main():
     print("Starting Asteroids")
     pygame.init()
@@ -10,12 +11,19 @@ def main():
     #Create Groups
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    #Set containers
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = updateable
+    asteroid_field = AsteroidField()
     #Setup Display 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updateable, drawable)
     Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # ✅ Create player object here
     while True:
